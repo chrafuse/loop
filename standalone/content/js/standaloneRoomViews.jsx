1e9a5f55f936fcbaf2b6e93afcb2f2bd5aa2b1e3
Chris Rafuse <chris.rafuse@priologic.com>
2016-02-29 09:07:59 -0800

    Bug 1245710 - link clicker intro screen

--- standalone/content/js/standaloneRoomViews.jsx
+++ standalone/content/js/standaloneRoomViews.jsx
@@ -599,6 +599,7 @@ loop.standaloneRoomViews = (function(mozL10n) {
       activeRoomStore: React.PropTypes.instanceOf(loop.store.ActiveRoomStore).isRequired,
       cursorStore: React.PropTypes.instanceOf(loop.store.RemoteCursorStore).isRequired,
       dispatcher: React.PropTypes.instanceOf(loop.Dispatcher).isRequired,
+      introSeen: React.PropTypes.bool,
       isFirefox: React.PropTypes.bool.isRequired,
       // The poster URLs are for UI-showcase testing and development
       localPosterUrl: React.PropTypes.string,
@@ -606,12 +607,26 @@ loop.standaloneRoomViews = (function(mozL10n) {
       roomState: React.PropTypes.string,
       screenSharePosterUrl: React.PropTypes.string
     },
-
     getInitialState: function() {
+      // test that it gets set -  remove after testing - uncomment next line to reset
+      // localStorage.removeItem("introSeen");
+      // Used by the UI showcase to override localStorage value to hide or show FTU slideshow.
+      // localStorage sourced data is always string or null
+      var introSeen = false;
+      if (this.props.introSeen !== undefined) {
+        if (this.props.introSeen) {
+          introSeen = true;
+        }
+      } else {
+        if (localStorage.getItem("introSeen") !== null) {
+          introSeen = true;
+        }
+      }
       var storeState = this.props.activeRoomStore.getStoreState();
       return _.extend({}, storeState, {
         // Used by the UI showcase.
-        roomState: this.props.roomState || storeState.roomState
+        roomState: this.props.roomState || storeState.roomState,
+        introSeen: introSeen
       });
     },
 
@@ -822,6 +837,7 @@ loop.standaloneRoomViews = (function(mozL10n) {
               roomState={this.state.roomState}
               roomUsed={this.state.used} />
           </sharedViews.MediaLayoutView>
+          {(this.state.introSeen) ? null : <IntroOverlayView joinRoom={this.joinRoom} />}
         </div>
       );
     }
@@ -850,6 +866,93 @@ loop.standaloneRoomViews = (function(mozL10n) {
       );
     }
   });
+
+  var Slide = React.createClass({
+    propTypes: {
+      imageClass: React.PropTypes.string.isRequired,
+      indexClass: React.PropTypes.string.isRequired,
+      joinRoom: React.PropTypes.func.isRequired,
+      text: React.PropTypes.string.isRequired,
+      title: React.PropTypes.string.isRequired
+    },
+
+    handleGotItClick: function(event) {
+      event.stopPropagation();
+      event.preventDefault();
+
+      // Code improvement for bug 1252306
+      // Duplicate code from handleCloseClick
+      // Remove from DOM by setState and render
+      var overlayElem = document.getElementsByClassName("intro-overlay")[0];
+      overlayElem.classList.add("hide");
+      // telemetry string data as to which button was clicked
+      localStorage.setItem("introSeen", "true");
+      this.props.joinRoom();
+    },
+
+    render: function() {
+      return (
+        <div className="slide">
+          <div className={this.props.indexClass}>
+            <div className="slide-layout">
+              <div className={this.props.imageClass} />
+              <h2>{this.props.title}</h2>
+              <div className="slide-text">{this.props.text}</div>
+              <button className="button-got-it" onClick={this.handleGotItClick}>
+                {mozL10n.get("intro_slide_button")}
+              </button>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  });
+
+  var IntroOverlayView = React.createClass({
+    propTypes: {
+      joinRoom: React.PropTypes.func.isRequired
+    },
+
+    handleCloseClick: function(event) {
+      event.stopPropagation();
+      event.preventDefault();
+
+      // Code improvement for bug 1252306
+      // Duplicate code from handleGotItClick
+      // Remove from DOM by setState and render
+      var overlayElem = document.getElementsByClassName("intro-overlay")[0];
+      overlayElem.classList.add("hide");
+      // telemetry string data as to which button was clicked
+      localStorage.setItem("introSeen", "true");
+      this.props.joinRoom();
+    },
+
+    render: function() {
+      var slideNode = {
+        id: "slide1",
+        imageClass: "slide1-image",
+        title: mozL10n.get("intro_slide_title"),
+        text: mozL10n.get("intro_slide_copy", {
+          clientSuperShortname: mozL10n.get("clientSuperShortname")
+        })
+      };
+      return (
+        <div className="intro-overlay">
+          <div className="slideshow">
+            <div className="slideshow-header">
+              <button className="button-close" onClick={this.handleCloseClick} />
+            </div>
+            <Slide imageClass={slideNode.imageClass}
+                   indexClass={slideNode.id}
+                   joinRoom={this.props.joinRoom}
+                   text={slideNode.text}
+                   title={slideNode.title} />
+            </div>
+        </div>
+      );
+    }
+  });
+
 
   var StandaloneRoomControllerView = React.createClass({
     mixins: [
